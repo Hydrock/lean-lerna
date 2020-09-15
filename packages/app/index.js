100f10e311f6a3f280ae1014fa4a395157a0dbf7
Alex Vechkanov <hydrock74@gmail.com>
2020-09-15 18:34:51 +0300

    test

---
+++ packages/app/index.js
@@ -0,0 +1,15 @@
+// packages/app/index.js
+const first = require('@monorepo/first');
+const second = require('@monorepo/second');
+
+const app = () => 'Hi from the app';
+
+const main = () => {
+  console.log(app());
+  console.log(first());
+  console.log(second());
+};
+
+main();
+
+module.exports = { app, main };
---
+++ packages/second/index.js
@@ -0,0 +1,4 @@
+// packages/second/index.js
+const second = () => 'Hi from the second module';
+
+module.exports = second;
